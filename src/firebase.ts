7b1d8259db246468fa4a010f3666c03a6fe9a94d
VGowthamTheja <vempalligowthamtheja@gmail.com>
2022-11-23 16:05:13 +0530

    control update

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,16 +4,16 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBv8oEIBP6tvCuWlIPRzfNI26YyQlbtFFU",
-  authDomain: "mychat-d8878.firebaseapp.com",
-  projectId: "mychat-d8878",
-  storageBucket: "mychat-d8878.appspot.com",
-  messagingSenderId: "144018301939",
-  appId: "1:144018301939:web:edd678b06ede1589fae886",
+  apiKey: process.env.REACT_APP_CHAT_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_CHAT_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_CHAT_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_CHAT_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_CHAT_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_CHAT_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const storage = getStorage();
-export const db = getFirestore()
+export const db = getFirestore();
